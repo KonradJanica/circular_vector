cfaf36319c1a526df315cf898406d5d352d50d60
KonradJanica <konradjanica@gmail.com>
2015-05-16 00:45:33 +0930

    Added driver

---
+++ main.cc
@@ -0,0 +1,22 @@
+#include "circularbuffer.h"
+
+#include <iostream>
+
+int main() {
+  circular_buffer<int> test;
+
+  // Test push back
+  for (int x = 0; x < 13; ++x) {
+    test.push_back(x);
+  }
+  for (size_t x = 0; x < test.size(); ++x) {
+    std::cout << test[x] << ",";
+  }
+  std::cout << std::endl;
+  
+  circular_buffer<int>::iterator it = test.begin();
+  while (it != test.end()) {
+    std::cout << *it << ",";
+    ++it;
+  }
+}
